bab8dba4c9ed493759d57f0ffda6c2add54b7d61
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-23 11:12:35 +0500

    echo bot done

--- echo.py
+++ echo.py
@@ -2,8 +2,23 @@ import telegram
 import os
 
 TOKEN = os.environ['TOKEN']
-
 bot = telegram.Bot(token=TOKEN)
-updates = bot.getUpdates()
-for update in updates:
-    print(update)
+
+def main():
+    last_update    = bot.getUpdates()[-1]
+    last_update_id = last_update.update_id
+
+    while True:
+        curr_update    = bot.getUpdates()[-1]
+        curr_update_id = curr_update.update_id
+
+        if last_update_id != curr_update_id:
+            chat_id = curr_update.message.from_user.id
+            text    = curr_update.message.text
+
+            if text:
+                bot.send_message(chat_id, text)
+            
+            last_update_id = curr_update_id
+
+main()
